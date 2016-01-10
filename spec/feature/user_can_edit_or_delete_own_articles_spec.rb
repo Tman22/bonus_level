9751c620a712a6e25254134ece26ef0f436c60c7
Joe Perry <joseph.w.perry@gmail.com>
2016-01-10 12:16:14 -0700

    Delete article functionality

--- spec/feature/user_can_edit_or_delete_own_articles_spec.rb
+++ spec/feature/user_can_edit_or_delete_own_articles_spec.rb
@@ -25,4 +25,23 @@ RSpec.feature "User can see articles" do
     expect(page).to have_content user.name
     expect(page).to_not have_content "body1"
   end
+
+  scenario "and they can delete their own" do
+    user = create(:user_with_articles)
+    article = user.articles.first
+
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+
+    visit articles_path
+    click_on article.title
+
+    current_path.should == article_path(article)
+    expect(page).to have_link "Delete", href: article_path(article)
+
+    click_on "Delete"
+
+    current_path.should == articles_path
+    expect(page).to_not have_content "Name1"
+  end
+
 end
